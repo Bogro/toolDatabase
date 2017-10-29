43eb39706b4bf6e6a33ffc27a4663d763f7f6e67
Christian Bogro <bogrolcr@gmail.com>
2017-10-29 21:15:40 +0000

    commit 29/10/17 n:2

---
+++ src/ExceptionDataBase.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 29/10/2017
+ * Time: 21:12
+ */
+
+namespace ToolDataBase;
+
+use \Exception;
+
+class ExceptionDataBase extends Exception
+{
+
+}
---
+++ src/ToolDataBase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LENOVO
+ * Date: 29/10/2017
+ * Time: 21:11
+ */
+
+namespace ToolDataBase;
+
+
+class ToolDataBase
+{
+
+}
